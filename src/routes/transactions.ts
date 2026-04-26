c2f6323ad9588f3eab73f0374198e7678bea3887
Eder Daniel Domingues <eder.infonet@gmail.com>
2026-04-26 15:21:05 -0400

    Criando transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,30 @@
 import type { FastifyInstance } from "fastify"
+import {z} from 'zod'
+import {randomUUID} from 'node:crypto'
 import { knex } from "../database"
 
 // Criar Rotas http
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const transactions = await knex("transactions")
-      .where("amount", 1000)
-      .select("*")
-    return transactions
+  app.post("/", async (request, reply) => {
+    // {title, amount, type: credit ou debit}
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const {title, amount, type} = 
+    createTransactionBodySchema.parse(
+      request.body
+    )
+
+    await knex('transactions')
+    .insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
